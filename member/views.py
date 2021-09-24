722b109ddc5323e01d4dc317a9fb683047021240
junjun97 <86711019+junjun97@users.noreply.github.com>
2021-09-24 23:32:51 +0900

    Add files via upload

--- member/views.py
+++ member/views.py
@@ -2,5 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def member(request):
-    return render(request, 'member.html')
+def member1(request):
+    return render(request, 'member1.html')
+
+
+
+def member2(request):
+    return render(request, 'member2.html')
+
+
+
+def member3(request):
+    return render(request, 'member3.html')
+
+
+
+def member4(request):
+    return render(request, 'member4.html')
